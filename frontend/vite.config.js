c0de1b73758a2dc6a7a61e6dd2405d26f61b909b
tech-rehalm <techrehalm@gmail.com>
2024-09-05 19:58:04 +0200

    fifth commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,13 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-<<<<<<< HEAD
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
-=======
-      "/api/": "https://e-commerce-backend-tawny.vercel.app/",
-      "/uploads/": "https://e-commerce-backend-tawny.vercel.app/",
->>>>>>> 52685171323f0d4f005d122e386f5daa726b79a3
+      "/api/": "https://e-commerce-backend-neon-tau.vercel.app/",
+      "/uploads/": "https://e-commerce-backend-neon-tau.vercel.app/",
     }
   }
 })
