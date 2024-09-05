a8e87f9680919a71bf54f4f9b36389c0d9d1af18
tech-rehalm <techrehalm@gmail.com>
2024-09-05 08:19:34 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://e-commerce-react-app-sepia.vercel.app/",
+      "/uploads/": "https://e-commerce-react-app-sepia.vercel.app/",
     }
   }
 })
